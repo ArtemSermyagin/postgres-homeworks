45e7c5f7ff29face1adfa3285fdf55d459d94f92
ArtemSermyagin <artem.sermyagin@yahoo.com>
2024-01-21 13:36:44 +0400

    домашка 1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,27 @@
 -- SQL-команды для создания таблиц
+
+-- Таблица для хранения информации о клиентах
+CREATE TABLE customers (
+    customer_id VARCHAR(255) PRIMARY KEY,
+    company_name VARCHAR(255),
+    contact_name VARCHAR(255)
+);
+
+-- Таблица для хранения информации о сотрудниках
+CREATE TABLE employees (
+    employee_id INT PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    title VARCHAR(255),
+    birth_date DATE,
+    notes TEXT
+);
+
+-- Таблица для хранения информации о заказах
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY,
+    customer_id VARCHAR(255) REFERENCES customers(customer_id),
+    employee_id INT REFERENCES employees(employee_id),
+    order_date DATE,
+    ship_city VARCHAR(255)
+);
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,35 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import psycopg2
+import csv
+
+database = "north"  # Название БД
+user = "postgres"  # Название пользователя БД
+password = "enoagnzc"  # сюда пароль от твоего пользователя
+
+data = {
+    "customers": "customers_data.csv",
+    "employees": "employees_data.csv",
+    "orders": "orders_data.csv",
+}
+# Путь к CSV-файлам
+csv_file_path = './north_data/'
+
+with psycopg2.connect(
+        host="localhost",
+        database=database,
+        user=user,
+        password=password
+) as conn:
+    with conn.cursor() as cursor:
+        for table, path in data.items():
+            with open(f"{csv_file_path}/{path}", 'r') as file:
+                reader = csv.DictReader(file)
+                fields = reader.fieldnames
+                insert_query = f"INSERT INTO {table} ({', '.join(fields)}) VALUES ({', '.join(['%s' for _ in fields])})"
+                for row in reader:
+                    cursor.execute(insert_query, tuple(row[field] for field in fields))
+    conn.commit()
+
+# INSERT INTO {table} ({', '.join(fields)}) VALUES ({', '.join(['%s' for _ in fields])})
+# INSERT INTO {table} ('customer_id','company_name','contact_name') VALUES ('%s','%','%s')
